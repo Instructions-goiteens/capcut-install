04d405959f64ba3cfaab89df9e663e563e873bdb
Instructions-goiteens <instructions.goiteens@gmail.com>
2025-02-27 15:05:43 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -297,10 +297,10 @@
             }));
         }
     }), 0);
-    const leaf1 = __webpack_require__.p + "demo-capcut/img/leaf1.webp";
-    const leaf2 = __webpack_require__.p + "demo-capcut/img/leaf2.webp";
-    const leaf3 = __webpack_require__.p + "demo-capcut/img/leaf3.webp";
-    const leaf4 = __webpack_require__.p + "demo-capcut/img/leaf4.webp";
+    const leaf1 = __webpack_require__.p + "capcut-install/img/leaf1.webp";
+    const leaf2 = __webpack_require__.p + "capcut-install/img/leaf2.webp";
+    const leaf3 = __webpack_require__.p + "capcut-install/img/leaf3.webp";
+    const leaf4 = __webpack_require__.p + "capcut-install/img/leaf4.webp";
     if (window.innerWidth > 549.98) {
         const leafImages = [ leaf1, leaf2, leaf3, leaf4 ];
         const maxLeaves = 15;
